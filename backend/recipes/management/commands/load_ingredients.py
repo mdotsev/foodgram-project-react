2b401d81da7a801fb7ec7a582b4d798f80bab579
mdotsev <m@dotsev.ru>
2022-11-21 11:13:10 +0300

    Debug=False

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -8,29 +8,35 @@ from django.db.utils import IntegrityError
 from recipes.models import Ingredient
 
 DATA_ROOT = os.path.join(settings.BASE_DIR, 'data')
+ALREDY_LOADED_ERROR_MESSAGE = 'Ингредиент уже есть в списке'
 
 
 class Command(BaseCommand):
-    help = 'loading ingredients from data in json'
+    help = 'Load ingredients from json'
 
     def add_arguments(self, parser):
-        parser.add_argument('filename', default='ingredients.json', nargs='?',
-                            type=str)
+        parser.add_argument(
+            'filename',
+            default='ingredients.json',
+            nargs='?',
+            type=str
+        )
 
     def handle(self, *args, **options):
         try:
-            with open(os.path.join(DATA_ROOT, options['filename']), 'r',
-                      encoding='utf-8') as f:
-                data = json.load(f)
+            with open(
+                os.path.join(DATA_ROOT, options['filename']),
+                'r', encoding='utf-8'
+            ) as dict:
+                data = json.load(dict)
                 for ingredient in data:
                     try:
-                        Ingredient.objects.create(name=ingredient["name"],
-                                                  measurement_unit=ingredient[
-                                                      "measurement_unit"])
+                        Ingredient.objects.create(
+                            name=ingredient['name'],
+                            measurement_unit=ingredient['measurement_unit']
+                        )
                     except IntegrityError:
-                        print(f'Ингридиет {ingredient["name"]} '
-                              f'{ingredient["measurement_unit"]} '
-                              f'уже есть в базе')
+                        print(ALREDY_LOADED_ERROR_MESSAGE)
 
         except FileNotFoundError:
             raise CommandError('Файл отсутствует в директории data')
