387da5b8e38758417adbf0288f7ed019b0ba8c10
mdotsev <m@dotsev.ru>
2022-11-22 18:56:57 +0300

    Paginator for tags off

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -126,8 +126,8 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 5,
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 5,
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
